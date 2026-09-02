d42d86c86cc608b0fa64d9587ce15f759ab543e2
eskezje <eskebn@gmail.com>
2026-09-02 11:13:14 +0200

    show hidden files in snack

--- shared/nvim/.config/nvim/lua/plugins/snacks.lua
+++ shared/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,6 +3,8 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        -- showing hidden files in snacks
+        hidden = true,
         sources = {
           explorer = {
             layout = {
